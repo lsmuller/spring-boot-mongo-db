d8cc6f64aceac479cf57c85ca30bca23f9e2453e
Leticia Muller <leticia.muller@sap.com>
2019-01-17 22:39:26 -0200

    Created project

---
+++ src/test/java/io/codementor/gtommee/springbootmongodb/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,17 @@
+package io.codementor.gtommee.springbootmongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMongoDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
